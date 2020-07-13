5377a0daebd26dcbdd1e76102a4225f6f1e073f3
Matt <matthieu.barbie@gmail.com>
2020-07-13 15:13:31 +0200

    Update songs.php

--- songs.php
+++ songs.php
@@ -5,12 +5,12 @@ include_once('database.php');
 $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATA, DB_PORT);
 $conn->set_charset("utf8");
 
+$query = "SELECT title, release_date, name FROM songs INNER JOIN artists ON songs.artist_id=artists.artist_id ORDER BY artists.name";
+
 if ($conn) {
-    if (isset($_POST['select'])) {
+    if (isset($_POST['sort'])) {
         $selectedVal = $_POST['select'];
-        $query = "SELECT * FROM songs ORDER BY $selectedVal";
-    }else{
-        $query = "SELECT * FROM songs ORDER BY artist_id";
+        $query = "SELECT title, release_date, name FROM songs INNER JOIN artists ON songs.artist_id=artists.artist_id ORDER BY $selectedVal";
     }
     $sendRequest = mysqli_query($conn, $query);
     $songsList = mysqli_fetch_all($sendRequest, MYSQLI_ASSOC);
@@ -32,12 +32,17 @@ if ($conn) {
     <h2>List of songs</h2>
     <form name="list" method="POST">
         <label for="selectId">Show by</label>
-        <select name="select" id="selectId">
-            <option value="artist_id">Artists Name</option>
+        <select name="select" id="selectId" value="<?= $selectedVal?>">
+            <option value="artists.name">Artists Name</option>
             <option value="title">Songs Name</option>
         </select>
         <input type="submit" name="sort" value="Sort By">
     </form>
+    <?php foreach($songsList as $song) : ?>
+        <h2><?= $song['title']?></h2>
+        <span>Artist  : <?= $song['name']?></span><br>
+        <span>Released date  : <?= $song['release_date']?></span><br>
+    <?php endforeach;?>
 
 </body>
 </html>
